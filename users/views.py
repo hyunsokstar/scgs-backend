0dba9d5c31caec42c73cee5742366dafbd6a04d0
hyun <hyunsokstar@gmail.com>
2023-03-05 16:58:13 +0900

    update for login error

--- users/views.py
+++ users/views.py
@@ -20,7 +20,7 @@ import jwt
 # Create your views here.
 class UserProfile(APIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
-    
+
     def get_object(self, pk):
         try:
             return User.objects.get(pk=pk)
@@ -32,14 +32,15 @@ class UserProfile(APIView):
         print(pk, type(pk))
         user = self.get_object(pk)
         # print("user : ", user)
-        serializer = serializers.UserProfileSerializer(user, context={"request":request})             
+        serializer = serializers.UserProfileSerializer(
+            user, context={"request": request})
         print("/users/:pk 에 대해 클라이언트로 넘너가는 데이터 serializer.data: ", serializer.data)
         return Response(serializer.data)
 
 
 class UserPhotos(APIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]    
-    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     def get_object(self, pk):
         try:
             return User.objects.get(pk=pk)
@@ -48,19 +49,19 @@ class UserPhotos(APIView):
 
     def post(self, request, pk):
         user = self.get_object(pk)  # 개별 유저 가져 오기
-        
+
         if request.user != user:
             raise PermissionDenied  # 현재 로그인한 사람이 아닐 경우 접근 거부
-        
+
         serializer = ProfilePhotoSerializer(data=request.data)
-        
+
         if serializer.is_valid():
-            
+
             exist_photo = PhotoForProfile.objects.filter(user=user)
-            if(exist_photo):
+            if (exist_photo):
                 result = exist_photo.delete()
                 print("result : ", result)
-            
+
             photo = serializer.save(user=user)
             print("photo : ", photo)
             serializer = ProfilePhotoSerializer(photo)
@@ -68,8 +69,8 @@ class UserPhotos(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-        
-        
+
+
 class Me(APIView):
 
     permission_classes = [IsAuthenticated]
@@ -78,7 +79,7 @@ class Me(APIView):
         user = request.user
         serializer = serializers.UserProfileSerializer(user)
         return Response(serializer.data)
-    
+
     def put(self, request):
         user = request.user
         serializer = serializers.PrivateUserSerializer(
@@ -92,8 +93,8 @@ class Me(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-        
-        
+
+
 class Users(APIView):
     def get(self, request):
         try:
@@ -103,37 +104,50 @@ class Users(APIView):
         except User.DoesNotExist:
             raise NotFound
         serializer = serializers.UserListSerializer(user, many=True)
-        return Response(serializer.data)    
-    
+        return Response(serializer.data)
+
     def post(self, request):
-        
+
         password = request.data.get("password")
         username = request.data.get("username")
-        
+        email = request.data.get("email")
+
+        userExists = User.objects.filter(username=username).exists()
+        emailExists = User.objects.filter(email=email).exists()
+
+        if (userExists):
+            raise ParseError("유저 네임이 이미 존재")
+
+        if (emailExists):
+            raise ParseError("이메일이 이미 존재")
+
         if not password:
-            raise ParseError("Password is missing")     
+            raise ParseError("Password is missing")
         if not username:
-            raise ParseError("username is missing")             
-        
+            raise ParseError("username is missing")
+
         serializer = serializers.PrivateUserSerializer(data=request.data)
-        
+
         if serializer.is_valid():
             user = serializer.save()
             user.set_password(password)
             user.save()
-            
-            new_user = authenticate(request, username=username, password=password)
+
+            new_user = authenticate(
+                request, username=username, password=password)
 
             serializer = serializers.PrivateUserSerializer(user)
             if new_user is not None:
                 login(request, new_user)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             else:
-                return Response({'message': 'Unable to log in with provided credentials.'}, status=status.HTTP_400_BAD_REQUEST)           
+                return Response({'message': 'Unable to log in with provided credentials.'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors)
 
 # /users/@{username}
+
+
 class PublicUser(APIView):
     def get(self, request, username):
         try:
@@ -142,8 +156,8 @@ class PublicUser(APIView):
             raise NotFound
         serializer = serializers.PrivateUserSerializer(user)
         return Response(serializer.data)
-    
-    
+
+
 # /users/change-password
 class ChangePassword(APIView):
     permission_classes = [IsAuthenticated]
@@ -159,14 +173,15 @@ class ChangePassword(APIView):
             user.save()
             return Response(status=status.HTTP_200_OK)
         else:
-            raise ParseError    
-        
+            raise ParseError
+
+
 class LogIn(APIView):
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
         if not username or not password:
-            raise ParseError
+            raise ParseError("username 이나 password 가 없습니다")
         user = authenticate(
             request,
             username=username,
@@ -176,7 +191,8 @@ class LogIn(APIView):
             login(request, user)
             return Response({"ok": "Welcome!"})
         else:
-            return ParseError("authentication info is wrong")
+            print("로그인 정보 틀렸음")
+            raise ParseError("authentication info is wrong")
 
 
 class LogOut(APIView):
@@ -185,8 +201,8 @@ class LogOut(APIView):
 
     def post(self, request):
         logout(request)
-        return Response({"ok": "bye!"}) 
-    
+        return Response({"ok": "bye!"})
+
 # class JWTLogIn(APIView):
 #     def post(self, request):
 #         username = request.data.get("username")
