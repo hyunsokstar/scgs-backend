0c4f44b72fc1bf0e6570985b42e7008d04140e31
hyun <hyunsokstar@gmail.com>
2023-04-11 14:19:03 +0900

    update for tag update

--- shortcut/admin.py
+++ shortcut/admin.py
@@ -1,11 +1,8 @@
 # shortcut1
 from django.contrib import admin
 from .models import ShortCut, Tags
-
-
 class TagsInline(admin.TabularInline):
     model = ShortCut.tags.through
-
 
 @admin.register(ShortCut)
 class ShortCutAdmin(admin.ModelAdmin):
