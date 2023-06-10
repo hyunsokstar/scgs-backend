e241b5f29e005159de0ea854c29d7480f5959dab
hyun <hyunsokstar@gmail.com>
2023-06-11 04:25:59 +0900

    update for note list

--- study_note/serializers.py
+++ study_note/serializers.py
@@ -3,12 +3,23 @@ from rest_framework import serializers
 from .models import StudyNote, StudyNoteContent
 
 
+# class StudyNoteSerializer(serializers.ModelSerializer):
+#     writer = UserProfileImageSerializer(read_only=True)
+
+#     class Meta:
+#         model = StudyNote
+#         fields = ['pk', 'title', 'description', 'writer']
 class StudyNoteSerializer(serializers.ModelSerializer):
     writer = UserProfileImageSerializer(read_only=True)
+    count_for_note_contents = serializers.SerializerMethodField()
 
     class Meta:
         model = StudyNote
-        fields = ['pk', 'title', 'description', 'writer']
+        fields = ['pk', 'title', 'description',
+                  'writer', 'count_for_note_contents']
+
+    def get_count_for_note_contents(self, obj):
+        return obj.note_contents.count()
 
 
 class StudyNoteContentSerializer(serializers.ModelSerializer):
