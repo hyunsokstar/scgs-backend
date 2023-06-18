24ad98bbe4e9c5c67de6731843a930a5788f5a87
hyun <hyunsokstar@gmail.com>
2023-06-18 09:17:15 +0900

    update for note comment count

--- study_note/serializers.py
+++ study_note/serializers.py
@@ -35,18 +35,34 @@ class CoWriterForStudyNoteSerializer(serializers.ModelSerializer):
         fields = ('id', 'writer', 'study_note', 'is_approved', 'created_at')
 
 
+# class StudyNoteSerializer(serializers.ModelSerializer):
+#     writer = UserProfileImageSerializer(read_only=True)
+#     note_cowriters = CoWriterForStudyNoteSerializer(many=True)
+#     count_for_note_contents = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = StudyNote
+#         fields = ['pk', 'title', 'description',
+#                   'writer', 'count_for_note_contents', 'note_cowriters']
+
+#     def get_count_for_note_contents(self, obj):
+#         return obj.note_contents.count()
 class StudyNoteSerializer(serializers.ModelSerializer):
     writer = UserProfileImageSerializer(read_only=True)
     note_cowriters = CoWriterForStudyNoteSerializer(many=True)
     count_for_note_contents = serializers.SerializerMethodField()
+    count_for_note_comments = serializers.SerializerMethodField()
 
     class Meta:
         model = StudyNote
         fields = ['pk', 'title', 'description',
-                  'writer', 'count_for_note_contents', 'note_cowriters']
+                  'writer', 'count_for_note_contents', 'note_cowriters', 'count_for_note_comments']
 
     def get_count_for_note_contents(self, obj):
         return obj.note_contents.count()
+
+    def get_count_for_note_comments(self, obj):
+        return obj.note_comments.count()
 
 
 class StudyNoteContentSerializer(serializers.ModelSerializer):
